3d2c248decfe6dedee6df6c2b0f109713836186f
Leonardo Vardiero <leovardiero@gmail.com>
2022-05-23 21:39:40 -0300

    Finish exercise 01

--- 01. IMC/assets/js/main.js
+++ 01. IMC/assets/js/main.js
@@ -1,19 +1,14 @@
 /* Auxiliary functions */
 
+// Calculates IMC and return as a Number
 const getIMC = function(peso, altura) {
   return Number((peso / (altura **2)).toFixed(2));
 };
 
-Number.prototype.between = function(a, b, inclusive) {
-  var min = Math.min(a, b),
-    max = Math.max(a, b);
-
-  return inclusive ? this >= min && this <= max : this > min && this < max;
-}
-
+// Given an IMC returns its message and level
 const getText = function(imc) {
   const imcResult = [
-    'Sub', 
+    'Abaixo do Peso', 
     'Peso Normal',
     'Sobrepeso',
     'Obesidade Grau I',
@@ -22,20 +17,63 @@ const getText = function(imc) {
   ];
 
   if(imc.between(0, 18.5,true)) {
-    return imcResult[0];
+    return [2, imcResult[0]];
   } else if (imc.between(18.5, 25, true)) {
-    return imcResult[1];
+    return [1, imcResult[1]];
   } else if (imc.between(25, 30, true)) {
-    return imcResult[2];
+    return [2, imcResult[2]];
   } else if (imc.between(30, 35, true)) {
-    return imcResult[3];
+    return [3, imcResult[3]];
   } else if (imc.between(35, 40, true)) {
-    return imcResult[4];
+    return [3, imcResult[4]];
   } else if (imc.between(45, 1000, true)) {
-    return imcResult[5];
+    return [3, imcResult[5]];
   };
 };
 
+// Clear the div
+const clearResult = function() {
+  const div = document.querySelector('.imcText');
+  div.innerHTML = '';
+}
+
+// Updates the IMC text
+const updateImcText = function(imc, imcTuple) {
+  const p = document.createElement('p');
+  const msg = `IMC: ${imc} | ${imcTuple[1]}`;
+  p.innerHTML = msg;
+
+  switch(imcTuple[0]) {
+    case 1: 
+      p.classList.add('isOk');
+      break;
+    case 2:
+      p.classList.add('isWarning');
+      break;
+    case 3:
+      p.classList.add('isDanger');
+      break;
+    default:
+      p.classList.add('isMessage');
+      break;
+  }
+
+  const div = document.querySelector('.imcText');
+  clearResult();
+
+  // console.log(p)
+  div.appendChild(p)
+}
+
+// Prototype for make easier calculate "between" two values
+Number.prototype.between = function(a, b, inclusive) {
+  var min = Math.min(a, b),
+    max = Math.max(a, b);
+
+  return inclusive ? this >= min && this <= max : this > min && this < max;
+}
+
+/* Button interruption */
 document.addEventListener('click', (e) => {
   // if it was not clicked on button, return
   if (!e.target.classList.contains('submit')) {
@@ -62,8 +100,7 @@ document.addEventListener('click', (e) => {
   }
 
   const imc = getIMC(peso, altura);
-  const imcText = getText(imc);
+  const imcTuple = getText(imc);
 
-  console.log(`${peso} | ${altura} | ${imc} | ${imcText}`);
-
+  updateImcText(imc, imcTuple);
 })
