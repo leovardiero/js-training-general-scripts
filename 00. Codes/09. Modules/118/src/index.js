8445349bdbaab442a73f000aa1e71718056814de
Leonardo Vardiero <leovardiero@gmail.com>
2022-07-25 22:03:10 -0300

    Update ModeloWebPack

---
+++ 00. Codes/09. Modules/118/src/index.js
@@ -0,0 +1,7 @@
+import * as mod1 from './module1'
+
+console.log(mod1)
+
+const p1 = new mod1.Pessoa('Leo', 'Vardiero');
+
+console.log(p1)
---
+++ 00. Codes/09. Modules/118/src/module1.js
@@ -0,0 +1,16 @@
+export const nome = 'Leo'
+export const sobrenome = 'Vardiero'
+export const idade = 30
+
+const cpf = '1243456789'
+
+export function sum(x,y) {
+  return x + y;
+};
+
+export class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome,
+    this.sobrenome = sobrenome
+  };
+};
